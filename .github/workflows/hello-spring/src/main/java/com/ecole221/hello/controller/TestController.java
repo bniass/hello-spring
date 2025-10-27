7b11c66c26deb0ecb00bdd91af742559e97ea8ec
Baye Niass <bniass@gmail.com>
2025-10-27 10:15:56 +0000

    first commit 1

---
+++ .github/workflows/hello-spring/src/main/java/com/ecole221/hello/controller/TestController.java
@@ -0,0 +1,19 @@
+package com.ecole221.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+    @GetMapping
+    public String index(){
+        return "Hello";
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello version 1.1";
+    }
+}
---
+++ .github/workflows/hello-spring/src/test/java/com/ecole221/hello/HelloSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecole221.hello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
